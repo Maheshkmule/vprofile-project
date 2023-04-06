052d909810c9b75bef1599fd3579925b45cd4486
Mahesh <107538554+Maheshkmule@users.noreply.github.com>
2023-04-06 16:09:45 +0530

    Update first_instance.tf

--- exercise1/first_instance.tf
+++ exercise1/first_instance.tf
@@ -15,3 +15,30 @@ resource "aws_instance" "intro" {
     Project = "Dove"
   }
 }
+
+
+/*
+  terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "ap-northeast-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0d979355d03fa2522"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+} 
+*/
